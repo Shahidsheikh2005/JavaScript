a6080b6aaee7122bd2d85cac822d4e793da96822
Shahid Sheikh <sheikhshahid8009@gmail.com>
2025-02-22 23:47:16 -0500

    js

---
+++ Events/event.js
@@ -0,0 +1,14 @@
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = () =>{
+//     console.log("btn1 was onclick");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// };
+// let div1 = document.querySelector("#div1");
+// div1.onmouseover = () =>{
+//     console.log("you are inside div");
+// };
+
+
+
